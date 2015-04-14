686d89645561023c2839b25a83a3f369d406edb2
Paul Nathan <nathanp1@tcnj.edu>
2015-04-14 09:31:25 -0400

    Implemented creating tickets for a project.

--- app/assets/stylesheets/https-::www.youtube.com:watch?v=B-NckB3CQ9o_files/XiFsXUbJSUO.js
+++
@@ -1,5 +0,0 @@
-/*!CK:2925241236!*//*1428703870,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["CcHQx"]); }
-
-__d("ShareNowCounter",["AsyncSignal","XShareNowCounterController"],function(a,b,c,d,e,f,g,h){b.__markCompiled&&b.__markCompiled();var i={logEvent:function(event){var j=h.getURIBuilder().setEnum('event',event).getURI();(new g(j)).send();}};e.exports=i;},null);
