95893447771a5e218f8b59b8d716dcccb92c580a
Paul Nathan <nathanp1@tcnj.edu>
2015-04-14 18:40:24 -0400

    finished chapter 7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150414200453) do
+ActiveRecord::Schema.define(:version => 20150414215807) do
 
   create_table "installs", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20150414200453) do
     t.integer  "project_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   add_index "tickets", ["project_id"], :name => "index_tickets_on_project_id"
--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -7,3 +7,10 @@ end
 Then(/^then I should not see "(.*?)"$/) do |string|
   expect(page).not_to have_content(string)
 end
+
+Given(/^"(.*?)" has created a ticket for this project:$/) do |email, table|
+  table.hashes.each do |attributes|
+  	attributes = attributes.merge!(:user => User.find_by_email!(email))
+  		@project.tickets.create!(attributes)
+  end
+end
