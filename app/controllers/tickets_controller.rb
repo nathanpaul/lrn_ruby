7be46877f866811d5f4dc78495eae0826c2acb77
Paul Nathan <nathanp1@tcnj.edu>
2015-04-14 09:30:21 -0400

    Added creating tickets.

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,33 @@
+class TicketsController < ApplicationController
+	before_filter :find_project
+	before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+
+	def new
+		@ticket = @project.tickets.build
+	end
+
+	def find_project
+		@project = Project.find(params[:project_id])
+	end
+
+	def create
+		@ticket = @project.tickets.build(params[:ticket])
+		if @ticket.save
+			flash[:notice] = "Ticket has been created."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Title can't be blank, so ticket has not been created."
+			render :action => "new"
+		end
+	end
+
+	def find_ticket
+		@ticket = @project.tickets.find(params[:id])
+	end
+
+	def show
+
+	end
+
+	private :find_project
+end
