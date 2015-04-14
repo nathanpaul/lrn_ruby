ce56238c452659c408a39131c383028fd4530ad0
Paul Nathan <nathanp1@tcnj.edu>
2015-04-14 08:38:57 -0400

    Going to a project that doesn't exist forces the user back to the projects page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ticketee::Application.initialize!
+
+Ticketee::Application.configure do
+	config.assets.compile = true
+end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe ProjectsController do
-
+	it "displays an error for a missing project" do
+		get :show, :id => "not-here"
+		#response.should reidrect_to(projects_path) old syntax
+		expect(response).to redirect_to(projects_path)
+		message = "The project you were looking for could not be found."
+		#flash[:alert].should eql(message) old syntax
+		expect(flash[:alert]).to eq(message)
+	end
 end
