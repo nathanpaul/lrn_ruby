00607aa52ee86843e45c1522e47779075e926e2f
Paul Nathan <nathanp1@tcnj.edu>
2015-04-19 11:30:06 -0400

    Allowed the creating of admin users through the admin backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,23 @@
 class Admin::UsersController < Admin::BaseController
+	before_filter :authorize_admin!
   def index
+  	@users = User.all(:order => "email")
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	#can't create dupe, need to set :admin and other protected attributes outside of the "new" method.
+	@user = User.new(params[:user].except(:admin))
+	@user.admin = params[:user][:admin] == "1"
+		if @user.save
+		  flash[:notice] = "User has been created."
+		  redirect_to admin_users_path
+		else
+		  flash[:alert] = "User has not been created."
+		  render :action => "new"
+		end
+	end
 end
